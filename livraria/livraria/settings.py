fda5bb5eb687c50d3e9c6fef7b35373bbabad4e1
KaueDeMoura <kaueoenningdmoura@gmail.com>
2022-06-23 09:20:56 -0300

    django

--- livraria/livraria/settings.py
+++ livraria/livraria/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=w#(a#45a297!*u*&j&p)p24cf7#1!hk%7!+vpkwd^y4vd1s*5'
+SECRET_KEY = 'django-insecure--@9o33$mb=!6l_t943auf&e30&0$=kdawzu7rabeqv)7+k&2^q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
